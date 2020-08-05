2bfc83ce9596ea7aa535bcc25c967cfbed22e52d
hermitty <47337808+hermitty@users.noreply.github.com>
2020-08-05 09:07:03 +0200

    Classifier init

--- GemFinder.Api/Program.cs
+++ GemFinder.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace GemFinder
+namespace GemFinder.Api
 {
     public class Program
     {
