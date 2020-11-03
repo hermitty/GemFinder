81f1f6f83f3bac57061986f19c69601c71d68bca
hermitty <wiktoria.pustelniak@gmail.com>
2020-11-03 10:36:03 +0100

    StoneService init

--- GemFinder.ImageProvider/Model/FileItem.cs
+++ GemFinder.ImageProvider/Model/FileItem.cs
@@ -7,13 +7,15 @@ namespace GemFinder.ImageProvider.Model
     public class FileItem
     {
         public Guid Id { get; }
+        public string FileName { get; set; }
         public string Label { get; }
         public string Source { get; }
         public string Path { get; set; }
-        public FileItem(Guid id, string label, string source, string path)
+        public FileItem(Guid id, string label, string source, string path, string fileName)
         {
             Id = id;
             Label = label;
+            FileName = fileName;
             Source = source;
             Path = path;
         }
---
+++ GemFinder.Services.Stones.Application/Queries/GetSingleImageStone.cs
@@ -0,0 +1,13 @@
+﻿using GemFinder.Services.Stones.Application.DTO;
+using GemFinder.Utils.CQRS.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GemFinder.Services.Stones.Application.Queries
+{
+    public class GetSingleImageStone : IQuery<SingleImageStoneDto>
+    {
+        public string Label { get; set; }
+    }
+}
