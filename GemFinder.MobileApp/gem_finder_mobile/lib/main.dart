85654983968e972f037ecc080ad638134fd14d84
hermitty <wiktoria.pustelniak@gmail.com>
2020-11-09 12:23:32 +0100

    Add user details view

--- GemFinder.MobileApp/gem_finder_mobile/lib/main.dart
+++ GemFinder.MobileApp/gem_finder_mobile/lib/main.dart
@@ -45,6 +45,7 @@ import 'package:flutter/material.dart';
 import 'package:gem_finder_mobile/view/list_view/list_view.dart';
 import 'package:gem_finder_mobile/view/side_menu/side_menu.dart';
 import 'package:gem_finder_mobile/view/sign_up/sign_up_view.dart';
+import 'package:gem_finder_mobile/view/user/user_details_view.dart';
 
 
 void main() {
@@ -70,9 +71,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: SideMenu(),
-      body: SignUpView()
-    );
+    return ProfilePage();
+    // return Scaffold(
+    //   drawer: SideMenu(),
+    //   body: SignUpView()
+    // );
   }
 }
